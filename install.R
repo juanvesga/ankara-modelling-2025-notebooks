5c168f45f8c79187ea3276e933ac48753ffebe92
Juan F. Vesga <juanovesga@gmail.com>
2025-09-08 19:27:39 +0100

    add install and runtime for kernell

---
+++ install.R
@@ -0,0 +1,39 @@
+# Install CRAN packages
+install.packages(c(
+    "readr",
+    "deSolve",
+    "gridExtra",
+    "ggplot2",
+    "dplyr", 
+    "tidyr",
+    "IRdisplay",
+    "IRkernel",
+    "reshape",
+    "remotes",
+    "readxl",
+    "outbreaks",
+    "incidence",
+    "distcrete",
+    "epitrix",
+    "magrittr",
+    "binom",
+    "ape",
+    "outbreaker2",
+    "here",
+    "devtools"  # More reliable than githubinstall
+))
+
+# Add drat repository and install packages
+drat:::add("mrc-ide")
+install.packages(c("dde", "odin"))
+
+# Install GitHub packages using remotes (more reliable than githubinstall)
+library(remotes)
+
+# Install GitHub packages
+install_github("annecori/EpiEstim")
+install_github("reconhub/projections")
+install_github("reconhub/epicontacts")
+
+# Note: epinow2 has heavy dependencies, might cause timeout
+# install_github("reconhub/epinow2")
